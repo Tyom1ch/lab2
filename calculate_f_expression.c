6e7406b134f82ffa29635274a369f3062369a597
Tyom1ch <tyomchbb@gmail.com>
2025-04-01 22:24:31 +0300

    edit2

--- calculate_f_expression.c
+++ calculate_f_expression.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
-#include <math.h>
 
 int main() {
-    int x = 3, y = 2; // Задаємо значення x та y
+  int x, y;
+  double term1, term2, term3, f;
 
-    double f = (double)(x + y) / (x * x + x * y + y * y) +
-               (double)x / (1 + y * y) +
-               (double)y / (1 + x * x);
+  printf("Введіть значення x: ");
+  scanf("%d", &x);
 
-    printf("f = %.2f\n", f);
+  printf("Введіть значення y: ");
+  scanf("%d", &y);
 
-    return 0;
+  // Обчислюємо окремі частини виразу
+  double denominator1 = (double)(x * x + x * y + y * y); //Знаменник для першого доданку
+
+  term1 = (double)(x + y) / denominator1;
+
+  term2 = (double)x / (1 + y * y);
+
+  term3 = (double)y / (1 + x * x);
+
+  // Обчислюємо значення функції
+  f = term1 + term2 + term3;
+
+  printf("Значення функції f: %lf\n", f);
+
+  return 0;
 }
